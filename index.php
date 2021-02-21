941a9616d9648b528316b6fdbf4b85d425c9aab7
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-02-21 15:39:08 +0200

    pradetas 11

--- index.php
+++ index.php
@@ -290,3 +290,23 @@ do {
 <h2>ND nr.11</h2>
 
 <?php
+
+echo ' Sugeneruokite masyvą, kurio ilgis atsitiktinai kinta nuo 10 iki 100. Masyvo reikšmes sudaro atsitiktiniai skaičiai 0-100 ir masyvai. Santykis skaičiuojamas atsitiktinai, bet taip, kad skaičiai sudarytų didesnę dalį nei masyvai. Reikšmių masyvų gylis nuo 1 iki 5, o reikšmės analogiškos (nuo 50% iki 100% atsitiktiniai skaičiai 0-100, o likusios masyvai) ir t.t. kol visos galutinės reikšmės bus skaičiai ne masyvai. Suskaičiuoti kiek elementų turi masyvas. Suskaičiuoti masyvo elementų (tie kurie ne masyvai) sumą. Suskaičiuoti maksimalų masyvo gylį. Atvaizduokite masyvą grafiškai . Masyvą pavazduokite kaip div elementą, kuris yra display:flex, kurio viduje yra skaičiai. Kiekvienas div elementas turi savo unikalų id ir unikalią background spalvą (spalva pvz nepavaizduota). pvz: ';
+echo '<br><br>';
+
+$masyvoIlgis = rand(10, 100);
+echo "masyvo ilgis $masyvoIlgis";
+echo '<br><br>';
+$length11A = rand(51, 99);
+echo "procentas virs 50% $length11A";
+echo '<br><br>';
+$chance11 = ($masyvoIlgis * $length11A)/100;
+$skaiciuKiekis = ceil($chance11);
+echo "skaiciu kiekis $skaiciuKiekis";
+echo '<br><br>';
+$masyvuKiekis = $masyvoIlgis - $skaiciuKiekis;
+echo "masyvu kiekis $masyvuKiekis";
+$array11 = [];
+for ($i=0; $i < $masyvoIlgis; $i++) { 
+    # code...
+}
